d232029d07aa0ee16a77d27df134cdbc541dfdbe
Alexa Whitney <112434021+AlexaWhit@users.noreply.github.com>
2022-09-07 14:33:48 -0600

    Add files via upload

---
+++ flashcards.py
@@ -0,0 +1,18 @@
+import json
+with open('me-capitals.json', 'r') as f:
+    data = json.load(f)
+
+total = len(data["cards"])
+score = 0
+
+for i in data["cards"]:
+    guess = input(i["q"] + " > ")
+
+    if guess.lower() == i["a"].lower():
+        score += 1
+        print(f"Correct! Current score: {score}/{total}")
+    else:
+        print("Incorrect! The correct answer was", i["a"])
+        print(f"Current score: {score}/{total}")
+
+print(f"Thank you for playing! You scored: {score} out of five correct!")
